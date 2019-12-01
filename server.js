f04bcf032975c121b10dd61c4ab274641fcd1a33
envalde <kohoutdanie@gmail.com>
2019-12-01 17:19:42 +0100

    55

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
@@ -7,12 +8,25 @@ const app = express();
 app.use(express.static("public"));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
   res.render("index", {
-    titel: "Home",
-    cssFile: "Hallo"
+    titel: "Homde"
   });
 });
 
-app.listen(3000);
+app.get("/test/:id", function(req, res, next) {
+  res.render("test", { output: req.params.id });
+});
+
+app.post("/ausgabe", (req, res, next) => {
+  res.render("ausgabe", {
+    ausgabe: req.body.id
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server läuft");
+});
