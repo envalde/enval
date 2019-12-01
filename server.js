e4398243b29e6047db72ef0ec543c9eccd781857
envalde <kohoutdanie@gmail.com>
2019-12-01 16:08:33 +0100

    stage1

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+
 var app = express();
 
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.render("index", {
+    titel: "Test",
+    test: "Hallo"
+  });
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+app.listen(3000);
