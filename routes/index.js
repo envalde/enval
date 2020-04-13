d4807c8dcbdef7b3875b1e8c3d5b638f9c1e54e8
envalde <kohoutdanie@gmail.com>
2020-04-13 21:32:53 +0200

    89

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
+var chat = require("../chatbot");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Enval" });
+router.get("/", function (req, res, next) {
+  elements = chat.getRandomElements(5);
+  res.render("index", { title: "Enval", chats: elements });
 });
 
 module.exports = router;
