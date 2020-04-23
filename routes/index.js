24316bb77ec8e2c3baf005f171694ee724bf6b0a
0kohout <0kohout@ziegler.de>
2020-04-23 08:13:02 +0200

    neu aufsetzen

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var chat = require("../chatbot");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  elements = chat.getRandomElements(5);
-  res.render("index", { title: "Enval", chats: elements });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-
-router.get('/login', function (req, res, next) {
-  res.render('login', {
-    title: 'login'
-  });
-});
 module.exports = router;
